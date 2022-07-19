912dd63fb11c5b91cbcbd62aa8c6e63bb7a44070
dragonSovereigntyGitHubPRIME-FIRE5948 <104478634+dragonSovereigntyGitHubPRIME-FIRE5948@users.noreply.github.com>
2022-07-19 10:28:44 +0800

    toString() update

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -131,10 +131,8 @@ public class ResourceCentre {
 
 		for (int i = 0; i < camcorderList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", camcorderList.get(i).getAssetTag(),
-					camcorderList.get(i).getDescription(), 
-					ResourceCentre.showAvailability(camcorderList.get(i).getIsAvailable()),
-					camcorderList.get(i).getDueDate(),camcorderList.get(i).getOpticalZoom());
+			output += camcorderList.get(i).toString();
+
 		}
 		return output;
 	}
